f5ce40c831c81a5e6eb61a0af310cf0c08d875d4
Alexandre Siqueira <alematema@gmail.com>
2018-05-15 16:04:53 -0300

    add regex validando formato aaaa/mm/dd

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,18 +1,21 @@
 class DateHelper {
 
 	
-	
 	constructor(){
 		throw new Error('Classe DateHelper não pode ser instanciada !!!');
 	}
 
-	static toDate(texto) {
-		return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+	static toDate(text) {
+		
+		if( ! /\d{4}-\d{2}-\d{2}/.test(text) )
+			throw new Error(text + ' deve estar no formato aaaa/mm/dd');
+		
+		return new Date(...text.split('-').map((item, indice) => item - indice % 2));
 	}
 
 
-	static toText(data) {
-		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+	static toText(date) {
+		return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 	}
 
 
